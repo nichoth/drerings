f1b03aceab0892688231c99f8613b3373aa07a37
nichoth <nichoth@nichoth.com>
2026-02-22 14:24:14 -0800

    add submit button

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,20 +3,21 @@ import { useRef, useEffect } from 'preact/hooks'
 import { type FunctionComponent } from 'preact'
 import Atrament from '@substrate-system/atrament'
 import fill from '@substrate-system/atrament/fill?worker'
+import { useSignal } from '@preact/signals'
 import './home.css'
+import { Button } from '../components/button'
 import Debug from '@substrate-system/debug'
 const debug = Debug('drerings:view')
 
 export const HomeRoute:FunctionComponent = function HomeRoute () {
     const sketchpad = useRef<HTMLCanvasElement>(null)
-    debug('the sketchpad...', sketchpad.current)
+    const isCanvasDirty = useSignal<boolean>(false)
 
     useEffect(() => {
         debug('sketchpad.current', sketchpad.current)
         if (!sketchpad.current) return
         const canvas = sketchpad.current
 
-        // instantiate Atrament
         const atrament = new Atrament(sketchpad.current, {
             width: canvas.offsetWidth,
             height: canvas.offsetHeight,
@@ -24,6 +25,14 @@ export const HomeRoute:FunctionComponent = function HomeRoute () {
             fill,
         })
         atrament.smoothing = 0.7
+
+        atrament.addEventListener('dirty', () => {
+            isCanvasDirty.value = true
+        })
+
+        atrament.addEventListener('clean', () => {
+            isCanvasDirty.value = false
+        })
     }, [sketchpad.current])
 
     return html`<div class="route home">
@@ -32,5 +41,16 @@ export const HomeRoute:FunctionComponent = function HomeRoute () {
         </p>
 
         <canvas ref=${sketchpad} id="sketchpad"></canvas>
+
+        <form>
+            <div class="controls">
+                <${Button}
+                    type="submit"
+                    disabled=${!isCanvasDirty.value}
+                >
+                    Post It
+                <//>
+            </div>
+        </form>
     </div>`
 }
